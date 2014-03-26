0685f624e57d7d7ec46263dd2d7f636f2c345754
cheddar <jmg2967@rit.edu>
2014-03-25 22:55:37 -0400

    Working on matching still, almost there, but not quite

--- spotify2playmusic.py
+++ spotify2playmusic.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python -u
 import spotify
 from gmusicapi import Mobileclient
 from os import system
@@ -83,24 +83,86 @@ def get_playlist(spot):
             print("Invalid option, " + option + " try again")
     if option == 0:
         print("Bye!")
+        spot.flush_caches()
+        spot.logout()
         exit()
     return spotify_playlists[option-1]
 
 def is_similar(title1, artist1, album1, title2, artist2, album2):
-    title1alt = title1.split("(")[0].split("[")[0]
-    artist1alt = artist1.split("(")[0].split("[")[0]
-    album1alt = album1.split("(")[0].split("[")[0]
-    title2alt = title2.split("(")[0].split("[")[0]
-    artist2alt = artist2.split("(")[0].split("[")[0]
-    album2alt = album2.split("(")[0].split("[")[0]
+    threshold1 = 15
+    threshold2 = 10
+    try:
+        title1alt = title1.split("(")[0].split("[")[0]
+        title2alt = title2.split("(")[0].split("[")[0]
+    except:
+        return False
+    try:
+        album2alt = album2.split("(")[0].split("[")[0]
+        album1alt = album1.split("(")[0].split("[")[0]
+    except:
+        album1, album2, album1alt, album2alt = "none", "none", "none", "none"
+    try:
+        artist1alt = artist1.split("(")[0].split("[")[0]
+        artist2alt = artist2.split("(")[0].split("[")[0]
+    except:
+        artist1, artist2, artist1alt, artist2alt = "none", "none", "none", "none"
 
-    if title1 is title2 and artist1 is artist2:
+    if len(artist1) < 10 or len(artist1alt) < 10:
+        threshold1 -= 2
+        threshold2 -= 1
+
+    if len(artist2) < 10 or len(artist2alt) < 10:
+        threshold1 -= 2
+        threshold2 -= 1
+
+    if len(title1) < 10 or len(title1alt) < 10:
+        threshold1 -= 2
+        threshold2 -= 1
+
+    if len(title2) < 10 or len(title2alt) < 10:
+        threshold1 -= 2
+        threshold2 -= 1
+
+    if len(album1) < 10 or len(album1alt) < 10:
+        threshold1 -= 2
+        threshold2 -= 1
+
+    if len(album2) < 10 or len(album2alt) < 10:
+        threshold1 -= 2
+        threshold2 -= 1
+
+    if len(artist1) > 25 or len(artist1alt) > 25:
+        threshold1 -= 4
+        threshold2 -= 2
+
+    if len(artist2) > 25 or len(artist2alt) > 25:
+        threshold1 -= 4
+        threshold2 -= 2
+
+    if len(title1) > 25 or len(title1alt) > 25:
+        threshold1 -= 4
+        threshold2 -= 2
+
+    if len(title2) > 25 or len(title2alt) > 25:
+        threshold1 -= 4
+        threshold2 -= 2
+
+    if len(album1) > 25 or len(album1alt) > 25:
+        threshold1 -= 4
+        threshold2 -= 2
+
+    if len(album2) > 25 or len(album2alt) > 25:
+        threshold1 -= 4
+        threshold2 -= 2
+
+
+    if title1 is title2 and artist1 is artist2 and not artist2 is "none":
         return True
-    elif title1alt is title2alt and artist1alt is artist2alt:
+    elif title1alt is title2alt and artist1alt is artist2alt and not artist2alt is "none":
         return True
-    elif (levenshtein(title1,title2) + levenshtein(artist1, artist2) + levenshtein(album1, album2) < 10):
+    elif (levenshtein(title1,title2) + levenshtein(artist1, artist2) + levenshtein(album1, album2) < threshold1):
         return True
-    elif (levenshtein(title1alt,title2alt) < 5 and levenshtein(artist1alt, artist2alt) < 5 and levenshtein(album1alt, album2alt) < 5):
+    elif (levenshtein(title1alt,title2alt) < threshold2 and levenshtein(artist1alt, artist2alt) < threshold2 and levenshtein(album1alt, album2alt) < threshold2):
         return True
     else:
         return False
@@ -135,7 +197,7 @@ def main():
         artist = track.artists[0].load().name
         album = track.album.load().name
         stdout.flush()
-        stdout.write("%i%% - %s - %s" % (((float(progress) / float(len(playlist.load().tracks))) * 100.0), title, artist))
+        stdout.write("%i%% - %s - %s     \r" % (((float(progress) / float(len(playlist.load().tracks))) * 100.0), title, artist))
         unmatched = True
         for i in gpm_local:
             if is_similar(title, artist, album, i['title'], i['artist'], i['album']):
@@ -145,7 +207,6 @@ def main():
                 break
         if unmatched:
             unmatched_tracks.append(track)
-        stdout.write("\b" * 500)
 
     print("Now searching All Access for the remaining tracks")
     progress = 0
@@ -157,18 +218,17 @@ def main():
         album = track.album.load().name
         query = title + " " + artist
         stdout.flush()
-        stdout.write("%i%% - %s - %s" % (((float(progress) / float(len(playlist.load().tracks))) * 100.0), title, artist))
+        stdout.write("%i%% - %s - %s     \r" % (((float(progress) / float(len(playlist.load().tracks))) * 100.0), title, artist))
         unmatched = True
         try:
             result = gpm.search_all_access(query, max_results = 1)['song_hits'][0]['track']
             if is_similar(title, artist, album, result['title'], result['artist'], result['album']):
-                print("- Found a match in All Access: " + result['title'] + " - " + result['artist'])
+                print(" - Found a match in All Access: " + result['title'] + " - " + result['artist'])
                 unmatched = False
                 unmatched_tracks.remove(track)
                 to_add_list.append(result['nid'])
         except:
             pass
-        stdout.write("\b" * 500)
 
     gpm_new_playlist_id = gpm.create_playlist(playlist.load().name)
     print("Adding matched songs to playlist\n")
