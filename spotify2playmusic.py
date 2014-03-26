144d169ac75835c98215cf0c91bff8bf9388a3bd
cheddar <jmg2967@rit.edu>
2014-03-26 02:40:48 -0400

    Added comments and finalized(?) matching algorithm

--- spotify2playmusic.py
+++ spotify2playmusic.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python -u
+#this tool looks up spotify playlists and clones them over to google play music
+#with an acceptable degree of accuracy allowing for variations in song info
+#this causes it to be pretty slow, but at least you don't have to do it by hand
+#read more at the README!
+
 import spotify
 from gmusicapi import Mobileclient
 from os import system
@@ -7,6 +12,7 @@ from sys import stdout
 
 def levenshtein(a,b):
 #from http://hetland.org/coding/python/levenshtein.py
+#used to compare strings, similar strings return lower numbers
     n, m = len(a), len(b)
     if n > m:
         a,b = b,a
@@ -22,24 +28,27 @@ def levenshtein(a,b):
             current[j] = min(add, delete, change)
     return current[n]
 
+#help handle spotify login
 logged_in_event = threading.Event()
 def logged_in_listener(session, error_type):
     logged_in_event.set()
 
 def get_password(prompt):
     #from http://scrollingtext.org/hiding-keyboard-input-screen
+    #allows entry of password from the terminal without it printing to the screen
     system("stty -echo")
     password = raw_input(prompt)
     system("stty echo")
     return password
 
 def login_gpm():
-    #username = raw_input("Enter your Google Play username: ")
-    #password = get_password("Enter your Google Play password: ")
-    file = open('play_creds', 'r')
-    username = file.readline().rstrip('\n')
-    password = file.readline().rstrip('\n')
-    file.close()
+    #takes credentials and checks user login of Google
+    username = raw_input("Enter your Google Play username: ")
+    password = get_password("Enter your Google Play password: ")
+    #file = open('play_creds', 'r')
+    #username = file.readline().rstrip('\n')
+    #password = file.readline().rstrip('\n')
+    #file.close()
     gpm = Mobileclient()
     if not gpm.login(username, password):
         print("\nNot a valid login")
@@ -49,12 +58,13 @@ def login_gpm():
         return gpm
 
 def login_spotify(spot):
-    #username = raw_input("Enter your Spotify username: ")
-    #password = get_password("Enter your Spotify password: ")
-    file = open('spotify_creds', 'r')
-    username = file.readline().rstrip('\n')
-    password = file.readline().rstrip('\n')
-    file.close()
+    #takes credentials and checks user login of spotify
+    username = raw_input("Enter your Spotify username: ")
+    password = get_password("Enter your Spotify password: ")
+    #file = open('spotify_creds', 'r')
+    #username = file.readline().rstrip('\n')
+    #password = file.readline().rstrip('\n')
+    #file.close()
     spot.login(username, password)
     spot.on(spotify.SessionEvent.LOGGED_IN, logged_in_listener)
     while not logged_in_event.wait(0.1):
@@ -67,6 +77,7 @@ def login_spotify(spot):
         return spot
 
 def get_playlist(spot):
+    #gets a list of all spotify playlists and allows the user to choose which one they want to clone
     playlists = spot.playlist_container
     playlists.load()
     spotify_playlists = []
@@ -83,87 +94,99 @@ def get_playlist(spot):
             print("Invalid option, " + option + " try again")
     if option == 0:
         print("Bye!")
-        spot.flush_caches()
         spot.logout()
         exit()
     return spotify_playlists[option-1]
 
 def is_similar(title1, artist1, album1, title2, artist2, album2):
-    threshold1 = 13
+    #the main comparison algorithm os the program
+    #makes the judgements on whether a song is a close enough match or not
+
+    #two thresholds explained later
+    threshold1 = 14
     threshold2 = 10
+
+    #the next few statements attempt to remove extra stuff from songs
+    #"The City (Extended Mix)" may not match, so we have an alternate for each field that removes
+    #things in brackets, parentheses, and things after dashes
     try:
-        title1alt = title1.split("(")[0].split("[")[0]
-        title2alt = title2.split("(")[0].split("[")[0]
+        title1alt = title1.split("(")[0].split("[")[0].split["-"][0]
+        title2alt = title2.split("(")[0].split("[")[0].split["-"][0]
     except:
+        #if the song does not have a title then just return fals
         return False
     try:
-        album2alt = album2.split("(")[0].split("[")[0]
-        album1alt = album1.split("(")[0].split("[")[0]
+        album2alt = album2.split("(")[0].split("[")[0].split["-"][0]
+        album1alt = album1.split("(")[0].split("[")[0].split["-"][0]
     except:
+        #if one of the parts of the comparison is blank then just make everything the same
         album1, album2, album1alt, album2alt = "none", "none", "none", "none"
     try:
-        artist1alt = artist1.split("(")[0].split("[")[0]
-        artist2alt = artist2.split("(")[0].split("[")[0]
+        artist1alt = artist1.split("(")[0].split("[")[0].split["-"][0]
+        artist2alt = artist2.split("(")[0].split("[")[0].split["-"][0]
     except:
+        #same as above
         artist1, artist2, artist1alt, artist2alt = "none", "none", "none", "none"
 
-    if len(title1) < 7 or len(title1alt) < 7:
+    #the next 16 statements modify the threshold according to the name length
+    #this is to allow songs with shorter details to have less variability and 
+    #songs with longer details to have higher variablility
+    #will probably replace with ratio based on length so the threshold
+    #is dynamically modified
+    if (len(title1) < 8 or len(title1alt) < 8):
         threshold1 -= 1
         threshold2 -= 1
-
-    if len(title2) < 7 or len(title2alt) < 7:
+    if len(title2) < 8 or len(title2alt) < 8:
         threshold1 -= 1
         threshold2 -= 1
-
+    if (len(title1) < 5 or len(title1alt) < 5):
+        threshold1 -= 1
+    if len(title2) < 5 or len(title2alt) < 5:
+        threshold1 -= 1
     if len(artist1) < 12 or len(artist1alt) < 12:
-        threshold1 -= 2
+        threshold1 -= 3
         threshold2 -= 1
-
     if len(artist2) < 12 or len(artist2alt) < 12:
-        threshold1 -= 2
+        threshold1 -= 3
         threshold2 -= 1
-
     if len(title1) < 12 or len(title1alt) < 12:
-        threshold1 -= 2
+        threshold1 -= 3
         threshold2 -= 1
-
     if len(title2) < 12 or len(title2alt) < 12:
-        threshold1 -= 2
+        threshold1 -= 3
         threshold2 -= 1
-
     if len(album1) < 12 or len(album1alt) < 12:
-        threshold1 -= 2
+        threshold1 -= 3
         threshold2 -= 1
-
     if len(album2) < 12 or len(album2alt) < 12:
-        threshold1 -= 2
+        threshold1 -= 3
         threshold2 -= 1
-
     if len(artist1) > 25 or len(artist1alt) > 25:
-        threshold1 += 3
-        threshold2 += 2
-
+        threshold1 += 2
+        threshold2 += 1
     if len(artist2) > 25 or len(artist2alt) > 25:
-        threshold1 += 3
-        threshold2 += 2
-
+        threshold1 += 2
+        threshold2 += 1
     if len(title1) > 25 or len(title1alt) > 25:
-        threshold1 += 3
-        threshold2 += 2
-
+        threshold1 += 2
+        threshold2 += 1
     if len(title2) > 25 or len(title2alt) > 25:
-        threshold1 += 3
-        threshold2 += 2
-
+        threshold1 += 2
+        threshold2 += 1
     if len(album1) > 25 or len(album1alt) > 25:
-        threshold1 += 3
-        threshold2 += 2
-
+        threshold1 += 2
+        threshold2 += 1
     if len(album2) > 25 or len(album2alt) > 25:
-        threshold1 += 3
-        threshold2 += 2
+        threshold1 += 2
+        threshold2 += 1
 
-
+    #this is the main comparison logic
+    #first compares the two for an exact match based off of title and artist
+    #next it uses the alternate titles and artists in a direct comparison
+    #third it takes the sum of the levenstein calculations for the differences in the
+    #title, artist, and album and compares it to the first threshold
+    #finally levenstien calculations are done for the alternatives and compared against
+    #the thresholds individually
     if title1 is title2 and artist1 is artist2 and not artist2 is "none":
         return True
     elif title1alt is title2alt and artist1alt is artist2alt and not artist2alt is "none":
@@ -173,18 +196,23 @@ def is_similar(title1, artist1, album1, title2, artist2, album2):
     elif (levenshtein(title1alt,title2alt) < threshold2 and levenshtein(artist1alt, artist2alt) < threshold2 and levenshtein(album1alt, album2alt) < threshold2):
         return True
     else:
+        #if there is no match then the function returns false
         return False
 
 def main():
+    #setup spotify config and initialize session
     config = spotify.Config()
     config.user_agent = 'spotify2playmusic'
     config.tracefile = b'/tmp/libspotify-trace.log'
     spot = spotify.Session(config=config)
 
+    #log in and get playlist choice for cloning
     gpm = login_gpm()
     spot = login_spotify(spot)
     playlist = get_playlist(spot)
 
+    #get google play playlist names and compare them to the chosen list
+    #if there is a playlist with the same name already in google play then say so and exit
     gpm_playlist_names = []
     for gpm_playlist in gpm.get_all_playlists():
         gpm_playlist_names.append(gpm_playlist['name'])
@@ -192,9 +220,12 @@ def main():
         print("Playlist already exists in Google Play... exiting")
         exit()
 
+    #there will be two lists: one of tracks to be added and another
+    #of songs that could not be matched
     to_add_list = []
     unmatched_tracks = []
 
+    #searches user uploaded music first
     print("Searching in uploaded music first...")
     gpm_local = gpm.get_all_songs()
     progress = 0
@@ -204,18 +235,33 @@ def main():
         title = track.name
         artist = track.artists[0].load().name
         album = track.album.load().name
+        #for progress
         stdout.flush()
         stdout.write("%i%% - %s - %s     \r" % (((float(progress) / float(len(playlist.load().tracks))) * 100.0), title, artist))
         unmatched = True
+        top3 = {}
+        to_push = ""
+        #checks against every song in the google play library and finds the
+        #top 3 hits, then the one with the lowest levenshtein number is chosen
         for i in gpm_local:
             if is_similar(title, artist, album, i['title'], i['artist'], i['album']):
-                print(" - Found a match in uploaded library: " + i['title'] + " - " + i['artist'])
+                print(" - Found a match in uploaded library: " + i['title'] + " - " + i['artist'] + "     ")
                 unmatched = False
-                to_add_list.append(i['id'])
+                top3[i['title']] = i['id']
+            if len(top3) == 3:
                 break
+            lowest_score = 999999
+        if len(top3) > 1:
+            for top in top3.keys():
+                val = levenshtein(title, top)
+                if val < lowest_score:
+                    lowest_score = val
+                    to_push = top3[top]
         if unmatched:
             unmatched_tracks.append(track)
+        to_add_list.append(to_push)
 
+    #all remaining unmatched tracks are searched in All Access
     print("Now searching All Access for the remaining tracks...")
     progress = 0
     for track in unmatched_tracks:
@@ -224,26 +270,46 @@ def main():
         title = track.name
         artist = track.artists[0].load().name
         album = track.album.load().name
+        #only searches for title and artist because google play does not return
+        #the correct results when album is included
         query = title + " " + artist
+        #for progress
         stdout.flush()
         stdout.write("%i%% - %s - %s     \r" % (((float(progress) / float(len(playlist.load().tracks))) * 100.0), title, artist))
         unmatched = True
+        #does two comparisons: one with the album and one where they both have
+        #the same album, this will increase the chance of matching
+        #I trust google's search function :)
         try:
             result = gpm.search_all_access(query, max_results = 1)['song_hits'][0]['track']
             if is_similar(title, artist, album, result['title'], result['artist'], result['album']):
-                print(" - Found a match in All Access: " + result['title'] + " - " + result['artist'])
+                print(" - Found a match in All Access: " + result['title'] + " - " + result['artist'] + "     ")
                 unmatched = False
                 unmatched_tracks.remove(track)
                 to_add_list.append(result['nid'])
         except:
             pass
 
+        if len(unmatched_tracks) != 0 and unmatched == True:
+            try:
+                result = gpm.search_all_access(query, max_results = 1)['song_hits'][0]['track']
+                if is_similar(title, artist, "analbumthebest", result['title'], result['artist'], "analbumthebest"):
+                    print(" - Found a match in All Access: " + result['title'] + " - " + result['artist'] + "     ")
+                    unmatched = False
+                    unmatched_tracks.remove(track)
+                    to_add_list.append(result['nid'])
+            except:
+                pass
+
+    #add the new playlist and all of the identified songs songs to it
     gpm_new_playlist_id = gpm.create_playlist(playlist.load().name)
     print("Adding matched songs to playlist\n")
     for to_add in to_add_list:
         gpm.add_songs_to_playlist(gpm_new_playlist_id, to_add)
 
+    #show the unmatched tracks
     print("Unmatched tracks:")
     for unmatched_track in unmatched_tracks:
         print(unmatched_track.load().name + " - " + unmatched_track.load().artists[0].load().name)
+
 main()
